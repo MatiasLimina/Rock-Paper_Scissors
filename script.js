8020b39fca3ccf51ca7c4228ce7b2656ec1c6790
Matias <matiaslimina@gmail.com>
2024-09-24 09:21:28 -0300

    Create getComputerChoice function
    [Ticket: 2]

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("eaaaaa")
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3)+1;
+    let choice = ""
+    if(num = 1 ){
+         choice= "Scissors";
+    }
+    else if(num = 2){
+         choice = "Rock";
+    }
+    else {
+         choice = "Paper";
+    }
+    return choice
+        
+        
+}
+console.log(getComputerChoice())
