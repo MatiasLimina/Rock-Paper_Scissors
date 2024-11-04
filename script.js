349efe75c6222e905d650582fc47f9e697f5dace
Matias <matiaslimina@gmail.com>
2024-11-04 11:51:09 -0300

    Make the game play on click Move the function that plays one round so it activates when the buttons are pressed
    [Ticket: 18]

--- script.js
+++ script.js
@@ -117,6 +117,9 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
  
   button.addEventListener("click", () => {
+
+
+
     playRound(button.id,getComputerChoice());
   });
 });
